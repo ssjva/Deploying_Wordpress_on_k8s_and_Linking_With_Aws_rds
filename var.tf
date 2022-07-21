7d1dfc1e2e954a6073d3a49ae7822b2628287ca2
ankush chouhan <68639886+ssjva@users.noreply.github.com>
2022-07-21 09:00:38 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "mysql_db_name" {
+    type = string
+    description = "Please enter your database name"
+}
+
+variable "mysql_username" {
+    type = string
+    description = "Please enter your database username"
+}
+
+variable "mysql_password" {
+    type = string
+    description = "Please input mysql password"
+}
